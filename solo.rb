f16d8b8543b5aded26f83f8fcaeab2e17cf68e4d
Grant McInnes <gmcinnes@marsdd.com>
2013-03-27 11:48:30 -0400

    Add tools for running chef outside vagrant

---
+++ solo.rb
@@ -0,0 +1,14 @@
+cookbook_path ["./vendor/cookbooks", "./site-cookbooks"]
+role_path "./roles"
+log_level :info
+
+data_bag_path "./data_bags"
+
+
+http_proxy nil
+http_proxy_user nil
+http_proxy_pass nil
+https_proxy nil
+https_proxy_user nil
+https_proxy_pass nil
+no_proxy nil
