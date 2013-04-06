c7121e2e140d1fcfdee9b7f960945b21bfc00169
Grant McInnes <gmcinnes@marsdd.com>
2013-04-05 23:08:46 -0400

    Deploy monit unicorn workers

--- site-cookbooks/bootstrap/recipes/monit_definitions.rb
+++ site-cookbooks/bootstrap/recipes/monit_definitions.rb
@@ -7,3 +7,12 @@ template "/etc/monit/conf.d/nginx.conf" do
   mode "0644"
   source "nginx.conf"
 end
+
+[0..3].each do |n|
+  template "/etc/monit/conf.d/unicorn_worker_#{n}.conf" do
+    mode "0644"
+    source "unicorn_worker_#{n}.conf"
+  end
+
+end
+
