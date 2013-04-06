97b4657f97a01db4108723520576ed0e4c4c295b
Grant McInnes <gmcinnes@marsdd.com>
2013-04-05 23:30:20 -0400

    Tools to monitor delayed job

--- site-cookbooks/bootstrap/recipes/monit_definitions.rb
+++ site-cookbooks/bootstrap/recipes/monit_definitions.rb
@@ -8,11 +8,15 @@ template "/etc/monit/conf.d/nginx.conf" do
   source "nginx.conf"
 end
 
+template "/etc/monit/conf.d/delayed_job.conf" do
+  mode "0644"
+  source "delayed_job.conf"
+end
+
 (0..3).each do |n|
   template "/etc/monit/conf.d/unicorn_worker_#{n}.conf" do
     mode "0644"
     source "unicorn_worker_#{n}.conf"
   end
-
 end
 
