7ea6579547795b0303a1686f88b90dc25d588883
Grant McInnes <gmcinnes@marsdd.com>
2013-04-10 21:21:36 -0400

    Add resource monitoring stuff for monit

--- site-cookbooks/bootstrap/recipes/monit_definitions.rb
+++ site-cookbooks/bootstrap/recipes/monit_definitions.rb
@@ -7,3 +7,8 @@ template "/etc/monit/conf.d/delayed_job.conf" do
   mode "0644"
   source "delayed_job.conf"
 end
+
+template "/etc/monit/conf.d/resources.conf" do
+  mode "0644"
+  source "resources.conf"
+end
