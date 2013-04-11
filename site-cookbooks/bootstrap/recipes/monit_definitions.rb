9fe4767b6990d08391e8a3cc053ddedd9ed051b9
Grant McInnes <gmcinnes@marsdd.com>
2013-04-10 21:01:57 -0400

    Dont configure unicorn monitoring. Leave that to app deployment

--- site-cookbooks/bootstrap/recipes/monit_definitions.rb
+++ site-cookbooks/bootstrap/recipes/monit_definitions.rb
@@ -1,8 +1,3 @@
-template "/etc/monit/conf.d/unicorn.conf" do
-  mode "0644"
-  source "unicorn.conf"
-end
-
 template "/etc/monit/conf.d/nginx.conf" do
   mode "0644"
   source "nginx.conf"
@@ -12,11 +7,3 @@ template "/etc/monit/conf.d/delayed_job.conf" do
   mode "0644"
   source "delayed_job.conf"
 end
-
-(0..3).each do |n|
-  template "/etc/monit/conf.d/unicorn_worker_#{n}.conf" do
-    mode "0644"
-    source "unicorn_worker_#{n}.conf"
-  end
-end
-
