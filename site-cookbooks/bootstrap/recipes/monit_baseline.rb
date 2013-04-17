d57a9fd0a35d9f912350b8848675049375656c72
Grant McInnes <gmcinnes@marsdd.com>
2013-04-16 21:39:49 -0400

    Add broken up monit scripts

---
+++ site-cookbooks/bootstrap/recipes/monit_baseline.rb
@@ -0,0 +1,15 @@
+#
+# Cookbook Name:: bootstrap
+# Recipe:: monit_definitions 
+#
+# Copyright 2013, Gameimprovement 
+#
+# All rights reserved - Do Not Redistribute
+#
+# Add definitions for monit to monitor system wide resources,
+# such as total CPU consumed, total MEM consumed, etc. 
+template "/etc/monit/conf.d/resources.conf" do
+  mode "0644"
+  source "resources.conf"
+end
+
---
+++ site-cookbooks/bootstrap/recipes/monit_web.rb
@@ -0,0 +1,20 @@
+#
+# Cookbook Name:: bootstrap
+# Recipe:: monit_definitions 
+#
+# Copyright 2013, Gameimprovement 
+#
+# All rights reserved - Do Not Redistribute
+#
+# Add definitions for monit to monitor system wide resources,
+# such as total CPU consumed, total MEM consumed, etc. 
+template "/etc/monit/conf.d/nginx.conf" do
+  mode "0644"
+  source "nginx.conf"
+end
+
+template "/etc/monit/conf.d/delayed_job.conf" do
+  mode "0644"
+  source "delayed_job.conf"
+end
+
