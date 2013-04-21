5b9f904c46c11ed9d04dcfd3b07521d7f95d53b1
Grant McInnes <gmcinnes@marsdd.com>
2013-04-20 23:31:04 -0400

    Remove unused bootstrap cookbook

--- site-cookbooks/bootstrap/metadata.rb
+++
@@ -1,7 +0,0 @@
-name             'bootstrap'
-maintainer       'Brent Jubinville'
-maintainer_email 'brent@utilityboy.com'
-license          'All rights reserved'
-description      'Adds some bootstrapping tasks for Game Improvement'
-long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.0'
--- site-cookbooks/bootstrap/recipes/default.rb
+++
@@ -1,9 +0,0 @@
-#
-# Cookbook Name:: bootstrap
-# Recipe:: default
-#
-# Copyright 2013, YOUR_COMPANY_NAME
-#
-# All rights reserved - Do Not Redistribute
-#
-
